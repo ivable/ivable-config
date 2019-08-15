e0324cc482a4206ac0af5e46835948322196f742
willem evertse <willem.evertse@ivable.com>
2019-08-15 09:50:33 +0200

    init + sentry logback settings

---
+++ ivable_config/src/main/java/com/ivable/common/config/IvableConfigCartridge.java
@@ -0,0 +1,5 @@
+package com.ivable.common.config;
+
+public class IvableConfigCartridge extends com.intershop.beehive.core.capi.cartridge.Cartridge
+{
+}
---
+++ ivable_config/src/main/java/com/ivable/common/config/internal/modules/IvableConfigNamingModule.java
@@ -0,0 +1,12 @@
+package com.ivable.common.config.internal.modules;
+
+/**
+ * Contributes bindings and other configurations.
+ */
+public class IvableConfigNamingModule extends com.google.inject.AbstractModule
+{
+    @Override
+    protected void configure()
+    {
+    }
+}
